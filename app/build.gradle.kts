7fa47bbf5725bb3515617035e62ba7b4d415b29e
sandra <tiredwakoo@gmail.com>
2024-06-03 21:49:50 -0700

    create splash screen; added tasky minmap; added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
