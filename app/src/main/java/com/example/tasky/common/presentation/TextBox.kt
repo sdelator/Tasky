66e544aa29cc4cdd02af5255546c94944d862783
sandra <tiredwakoo@gmail.com>
2024-06-09 05:52:33 -0700

    remove bottom line in textField

--- app/src/main/java/com/example/tasky/common/presentation/TextBox.kt
+++ app/src/main/java/com/example/tasky/common/presentation/TextBox.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -26,6 +28,10 @@ fun TextBox(hintText: String) {
             .fillMaxWidth()
             .padding(top = 16.dp, start = 16.dp, end = 16.dp),
         shape = RoundedCornerShape(10.dp),
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Transparent, // Set focused indicator color to match background
+            unfocusedIndicatorColor = Color.Transparent // Set unfocused indicator color to match background
+        ),
         label = { Text(hintText) }
     )
 }
