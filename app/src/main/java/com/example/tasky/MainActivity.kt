41ca56a29710030608d3d045cc9bc9cf78ebf6e0
sandra <tiredwakoo@gmail.com>
2024-06-17 20:19:02 -0700

    working nav graph

--- app/src/main/java/com/example/tasky/MainActivity.kt
+++ app/src/main/java/com/example/tasky/MainActivity.kt
@@ -9,12 +9,20 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import com.example.tasky.feature_login.presentation.LoginScreenContent
 import com.example.tasky.feature_login.presentation.RegisterAccountContent
@@ -47,17 +55,15 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
 
             val startDestination: Any = if (isLoggedInState.value) {
-                LoginNav
-            } else RegisterNav
+                AgendaNav
+            } else AuthNavRoute
 
             TaskyTheme {
                 NavHost(navController, startDestination = startDestination) {
-                    composable<RegisterNav> {
-                        RegisterAccountContent(navController = navController)
+                    navigation<AuthNavRoute>(startDestination = RegisterNav) {
+                        authGraph(navController)
                     }
-                    composable<LoginNav> {
-                        LoginScreenContent(navController = navController)
-                    }
+
                     composable<AgendaNav> {
                         // todo create Composable
                         Box(
@@ -66,12 +72,38 @@ class MainActivity : ComponentActivity() {
                                 .background(Color.Green)
                         )
                     }
+
                     // todo add other screens
+//                    navigation(
+//                        startDestination = "agenda",
+//                        route = "calendar"
+//                    )
                 }
             }
         }
     }
 }
+
+@Composable
+inline fun <reified T : ViewModel> NavBackStackEntry.sharedViewModel(navController: NavController): T {
+    val navGraphRoute = destination.parent?.route ?: return viewModel()
+    val parentEntry = remember(this) {
+        navController.getBackStackEntry(navGraphRoute)
+    }
+    return viewModel(parentEntry)
+}
+
+fun NavGraphBuilder.authGraph(navController: NavController) {
+    composable<RegisterNav> {
+        RegisterAccountContent(navController = navController)
+    }
+    composable<LoginNav> {
+        LoginScreenContent(navController = navController)
+    }
+}
+
+@Serializable
+object AuthNavRoute
 
 @Serializable
 object RegisterNav
