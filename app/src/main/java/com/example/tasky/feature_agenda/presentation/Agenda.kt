42b2c551725a4bcbfd4f79a7b2309ecff47616a5
sandra <tiredwakoo@gmail.com>
2024-06-24 08:13:14 -0700

    create agenda UI composable skeleton and agenda toolbar

---
+++ app/src/main/java/com/example/tasky/feature_agenda/presentation/Agenda.kt
@@ -0,0 +1,56 @@
+package com.example.tasky.feature_agenda.presentation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+@Preview
+fun BoxWithAlignedContent() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .safeDrawingPadding()
+    ) {
+        AgendaToolbar()
+
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 75.dp),
+            shape = RoundedCornerShape(
+                topStart = 30.dp, topEnd = 30.dp
+            ),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState())
+                    .background(color = Color.Green),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Top Text")
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tasky/feature_agenda/presentation/AgendaToolbar.kt
@@ -0,0 +1,63 @@
+package com.example.tasky.feature_agenda.presentation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tasky.R
+
+@Composable
+@Preview
+fun AgendaToolbar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.Red)
+            .padding(start = 16.dp, end = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "March", color = Color.White)
+        ProfileIcon("AB")
+    }
+}
+
+@Composable
+fun ProfileIcon(
+    initials: String = "",
+) {
+    Box(
+        modifier = Modifier
+            .padding(top = 8.dp, bottom = 8.dp)
+            .background(color = colorResource(id = R.color.light_gray), shape = CircleShape)
+            .size(34.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = initials,
+            color = colorResource(id = R.color.light_blue),
+            fontSize = 13.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+@Preview
+fun PreviewProfileIcon() {
+    ProfileIcon("AB")
+}
