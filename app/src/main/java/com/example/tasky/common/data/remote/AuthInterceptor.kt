dcfe498d703374f45d26b461c9b1e7ab3ddc7a8a
sandra <tiredwakoo@gmail.com>
2024-06-24 21:01:27 -0700

    temp holder for accessToken

---
+++ app/src/main/java/com/example/tasky/common/data/remote/AuthInterceptor.kt
@@ -0,0 +1,18 @@
+package com.example.tasky.common.data.remote
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor : Interceptor {
+    companion object {
+        const val AUTH = "Authorization"
+    }
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val accessToken = ""
+        val request = chain.request().newBuilder()
+            .addHeader(AUTH, "Bearer $accessToken")
+            .build()
+        return chain.proceed(request)
+    }
+}
