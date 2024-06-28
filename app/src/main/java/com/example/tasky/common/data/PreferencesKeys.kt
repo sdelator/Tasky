ba529665756b4d8362e0f2f3321ba76ac6cd495d
sandra <tiredwakoo@gmail.com>
2024-06-28 16:20:59 -0700

    remove unused val

--- app/src/main/java/com/example/tasky/common/data/PreferencesKeys.kt
+++ app/src/main/java/com/example/tasky/common/data/PreferencesKeys.kt
@@ -1,12 +1,10 @@
 package com.example.tasky.common.data
 
 import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 
 object PreferencesKeys {
     val REFRESH_TOKEN = stringPreferencesKey("refresh_token")
     val NAME = stringPreferencesKey("name")
     val IS_LOGGED_IN = booleanPreferencesKey("is_logged_in")
-    val USER_AGE = intPreferencesKey("user_age")
 }
