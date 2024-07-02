a17b32bceda96de2b3a784916d5e847c12fc6fc4
sandra <tiredwakoo@gmail.com>
2024-07-02 13:19:37 -0700

    fix test cases

--- app/src/test/java/com/example/tasky/fakes/FakeSessionStateManager.kt
+++ app/src/test/java/com/example/tasky/fakes/FakeSessionStateManager.kt
@@ -1,11 +1,19 @@
 package com.example.tasky.fakes
 
 import com.example.tasky.common.domain.SessionStateManager
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class FakeSessionStateManager : SessionStateManager {
     private val refreshTokenFlow = MutableStateFlow<String?>(null)
     private val nameFlow = MutableStateFlow<String?>(null)
+    override suspend fun setAccessToken(accessToken: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAccessToken(): Flow<String?> {
+        TODO("Not yet implemented")
+    }
 
     override suspend fun setRefreshToken(refreshToken: String) {
         refreshTokenFlow.value = refreshToken
