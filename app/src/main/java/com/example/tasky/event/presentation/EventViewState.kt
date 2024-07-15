59e17ebd8717f5c993114a3eaac6eb39d8c6a695
sandra <tiredwakoo@gmail.com>
2024-07-15 09:21:26 -0700

    add viewMode to event view state

--- app/src/main/java/com/example/tasky/event/presentation/EventViewState.kt
+++ app/src/main/java/com/example/tasky/event/presentation/EventViewState.kt
@@ -8,9 +8,14 @@ data class EventViewState(
     val startDate: String = "",
     val endDate: String = "",
     val reminder: Int = 0,
+    val eventViewMode: EventViewMode = EventViewMode.EDITABLE,
     val filterSelected: Filter = Filter.ALL
 )
 
 enum class Filter(type: String) {
     ALL("All"), GOING("Going"), NOT_GOING("Not Going")
+}
+
+enum class EventViewMode {
+    NON_EDITABLE, EDITABLE, ATTENDEE, ATTENDEE_EDITABLE
 }
