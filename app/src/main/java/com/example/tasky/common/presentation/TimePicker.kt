bcb404d8be6801a307a6cfc44de09eb21ed9a81c
sandra <tiredwakoo@gmail.com>
2024-07-15 12:05:57 -0700

    add custom colors to timePicker

--- app/src/main/java/com/example/tasky/common/presentation/TimePicker.kt
+++ app/src/main/java/com/example/tasky/common/presentation/TimePicker.kt
@@ -1,10 +1,14 @@
 package com.example.tasky.common.presentation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tasky.R
 import com.vanpra.composematerialdialogs.MaterialDialog
+import com.vanpra.composematerialdialogs.datetime.time.TimePickerColors
+import com.vanpra.composematerialdialogs.datetime.time.TimePickerDefaults
 import com.vanpra.composematerialdialogs.datetime.time.timepicker
 import com.vanpra.composematerialdialogs.rememberMaterialDialogState
 
@@ -20,11 +24,26 @@ fun PreviewTimePicker() {
         }
     ) {
         timepicker(
-
+            colors = customTimePickerColors()
         ) {
             dialogState.hide()
         }
     }
     /* This should be called in an onClick or an Effect */
     dialogState.show()
+}
+
+@Composable
+fun customTimePickerColors(): TimePickerColors {
+    return TimePickerDefaults.colors(
+        activeBackgroundColor = colorResource(id = R.color.tasky_green),
+        inactiveBackgroundColor = colorResource(id = R.color.reminder_gray),
+        activeTextColor = Color.White,
+        inactiveTextColor = Color.Black,
+        inactivePeriodBackground = colorResource(id = R.color.gray),
+        selectorColor = colorResource(id = R.color.tasky_green),
+        selectorTextColor = Color.White,
+        headerTextColor = Color.Black,
+        borderColor = Color.Black
+    )
 }
