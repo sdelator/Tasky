539950a26d2b7988d1bac41217791df3b02e5a57
sandra <tiredwakoo@gmail.com>
2024-07-15 21:20:14 -0700

    remove unused ExperimentalMaterial3API

--- app/src/main/java/com/example/tasky/common/presentation/editing/Editing.kt
+++ app/src/main/java/com/example/tasky/common/presentation/editing/Editing.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -52,7 +51,6 @@ fun EditScreenContent() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditableFieldArea() {
     val text = remember { mutableStateOf("") }
