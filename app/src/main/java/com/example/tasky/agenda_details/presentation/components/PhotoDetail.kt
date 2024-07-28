5cc3bf5b11617a9239d05ef1b753360ed6281a93
sandra <tiredwakoo@gmail.com>
2024-07-28 14:27:17 -0700

    clean code

--- app/src/main/java/com/example/tasky/agenda_details/presentation/components/PhotoDetail.kt
+++ app/src/main/java/com/example/tasky/agenda_details/presentation/components/PhotoDetail.kt
@@ -18,11 +18,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -40,22 +35,13 @@ import coil.compose.AsyncImage
 import com.example.tasky.R
 import com.example.tasky.agenda_details.presentation.PhotoDetailAction
 import com.example.tasky.common.domain.Constants
-import kotlinx.coroutines.coroutineScope
 
 @Composable
 fun PhotoDetailRoot(
     navController: NavController,
     image: String
 ) {
-    var imageUri by remember { mutableStateOf<Uri?>(null) }
-
-    LaunchedEffect(image) {
-        coroutineScope {
-            imageUri = Uri.parse(image)
-        }
-    }
-
-    imageUri?.let {
+    Uri.parse(image)?.let {
         PhotoDetailContent(
             image = it,
             onClick = { toolbarAction ->
