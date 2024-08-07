6fb5ab994bda7647fd38ad2b464ef67f9c1ae2ee
sandra <tiredwakoo@gmail.com>
2024-08-07 15:19:19 -0700

    basic notification

--- app/src/main/java/com/example/tasky/TaskyApplication.kt
+++ app/src/main/java/com/example/tasky/TaskyApplication.kt
@@ -1,7 +1,11 @@
 package com.example.tasky
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.util.Log
+import com.example.tasky.common.presentation.ReminderNotificationService
 import dagger.hilt.android.HiltAndroidApp
 
 
@@ -14,5 +18,19 @@ class TaskyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Log.d(TAG, "OnCreate is called")
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            ReminderNotificationService.REMINDER_CHANNEL_ID,
+            "Reminder",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        channel.description = "Used for event reminders"
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
